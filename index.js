0d069b3dbd9efd4c3d587bb50ff95b94caacf017
Dani Schuhman <dani8439@aol.com>
2022-09-28 15:50:11 -0400

    Added event listeners.

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 var canvas = document.getElementById("draw");
 var ctx = canvas.getContext("2d");
 let color = "#000";
+let offsetX = canvas.offsetLeft;
+let offsetY = canvas.offsetTop;
 let brushthickness = 7;
-document.querySelector(".color-btn div").style.backgroundColor = color;
 
 const erase = () => (ctx.globalCompositeOperation = "destination-out");
+document.querySelector(".color-btn div").style.backgroundColor = color;
+
+resize();
 
 /* Toggle Brush Size Dropdown */
 function sizeList() {
     document.querySelector(".size-list").classList.toggle("show-list");
+    brushSize();
 }
 
 /* Set Brush Size */
@@ -44,6 +49,16 @@ function colorPick(){
     setActiveColor();
 }
 
+/* Set Cursor Position */
+// initialize position as 0,0
+var pos = { x: 0, y: 0 };
+
+// new position from mouse events
+function setPosition(e) {
+  pos.x = parseInt(e.clientX - offsetX);
+  pos.y = parseInt(e.clientY - offsetY);
+}
+
 /* Draw */
 
 function draw(e) {
@@ -51,7 +66,7 @@ function draw(e) {
 
     ctx.beginPath(); // begin the drawing path 
     ctx.lineWidth = brushthickness; // width of the line 
-    ctx.linecap = "rount"; // rounded end cap 
+    ctx.linecap = "round"; // rounded end cap 
     ctx.strokeStyle = color; // hex color of the line 
     ctx.moveTo(pos.x, pos.y); // from position 
     setPosition(e);
@@ -68,8 +83,6 @@ function resize() {
   ctx.canvas.width = window.innerWidth - 20;
   ctx.canvas.height = window.innerHeight;
 }
-
-resize();
 
 //*************************************************************************************************
 //************************************** DOWNLOAD CANVAS ******************************************
@@ -89,3 +102,13 @@ function onSave() {
 
 // add window event listener to trigger when window is resized
 window.addEventListener("resize", resize);
+
+// Add event listeners to trigger on different mouse events 
+document.addEventListener("mousemove", draw);
+document.addEventListener("mousedown", setPosition);
+document.addEventListener("mouseenter", setPosition);
+document.addEventListener("touchmove", draw);
+document.addEventListener("touchend", setPosition);
+document.addEventListener("touchstart", setPosition);
+document.getElementById("color-picker").addEventListener("change", colorPick);
+setColor();
