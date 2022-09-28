2a3342e3587f151ef671ae18ca032aa12a772981
Dani Schuhman <dani8439@aol.com>
2022-09-28 15:43:02 -0400

    added other functionality to draw following tutorial.

--- index.js
+++ index.js
@@ -4,6 +4,62 @@ let color = "#000";
 let brushthickness = 7;
 document.querySelector(".color-btn div").style.backgroundColor = color;
 
+const erase = () => (ctx.globalCompositeOperation = "destination-out");
+
+/* Toggle Brush Size Dropdown */
+function sizeList() {
+    document.querySelector(".size-list").classList.toggle("show-list");
+}
+
+/* Set Brush Size */
+function brushSize() {
+    var brushSet = document.getElementsByClassName("size");
+    Array.prototype.forEach.call(brushSet, function(element) {
+        element.addEventListener("click", function() {
+            brushthickness = element.getAttribute("style").substr(11, 2);
+        });
+    });
+}
+
+/* Set Color */
+
+function setActiveColor() {
+    document.querySelector(".color-btn div").style.backgroundColor = color
+    ctx.strokeStyle = color;
+    ctx.globalCompositeOperation = "source-over";
+}
+
+function setColor() {
+    var palette = document.getElementsByClassName("color");
+    Array.prototype.forEach.call(palette, function(element) {
+        element.addEventListener("click", function() {
+            color = element.getAttribute("style").split("--set-color:")[1];
+            setActiveColor();
+        });
+    });
+}
+
+function colorPick(){
+    color = document.getElementById("color-picker").value;
+    setActiveColor();
+}
+
+/* Draw */
+
+function draw(e) {
+    if (e.buttons !==1) return; // if the mouse is not clicked, don't go further 
+
+    ctx.beginPath(); // begin the drawing path 
+    ctx.lineWidth = brushthickness; // width of the line 
+    ctx.linecap = "rount"; // rounded end cap 
+    ctx.strokeStyle = color; // hex color of the line 
+    ctx.moveTo(pos.x, pos.y); // from position 
+    setPosition(e);
+    ctx.lineTo(pos.x, pos.y); // to position 
+    ctx.closePath();
+    ctx.stroke(); // draw it. 😃
+}
+
 //*************************************************************************************************
 //******************************************* RESIZE CANVAS ***************************************
 //*************************************************************************************************
